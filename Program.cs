c0acac07ff3a924dc342437e1cdd42f60e574e2b
Luong Chi Dung <princeflaga@gmail.com>
2023-09-22 08:26:07 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThuVien thuVien = new ThuVien();
+            thuVien.ThemSach("S001", "Anh", "Dung");
+            thuVien.ThemSach("S002", "Toan", "Tuan");
+            thuVien.ThemSach("S003", "Van", "Tu");
+
+            Console.WriteLine("Danh sach trong thu vien: ");
+            thuVien.HienThiThongTinSach();
+
+            string maSachCanXoa = "S002";
+            string ketQuaXoa = thuVien.XoaSach(maSachCanXoa);
+            Console.WriteLine(ketQuaXoa);
+
+            Console.WriteLine("Danh sach sau khi xoa:");
+            thuVien.HienThiThongTinSach();
+            Console.ReadKey();
+
+            
+        }
+        
+    }
+}
---
+++ Sach.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class Sach
+{
+    public string MaSach { get; set; }
+    public string TenSach { get; set; }
+    public string TacGia { get; set; }
+
+    public Sach(string maSach, string tenSach, string tacGia)
+    {
+        MaSach = maSach;
+        TenSach = tenSach;
+        TacGia = tacGia;
+    }
+}
+
+    internal class ThuVien
+{
+    private List<Sach> sachList;
+
+    public ThuVien()
+    {
+        sachList = new List<Sach>();
+    }
+
+    public void ThemSach(string maSach, string tenSach, string tacGia)
+    {
+        Sach sach = new Sach(maSach, tenSach, tacGia);
+        sachList.Add(sach);
+    }
+
+    public string XoaSach(string maSach)
+    {
+        for (int i = 0; i < sachList.Count; i++)
+        {
+            if (sachList[i].MaSach == maSach)
+            {
+                sachList.RemoveAt(i);
+                return $"Da xoa sach co ma {maSach}";
+            }
+        }
+        return $"Khong tim thay sach co ma {maSach}";
+    }
+
+    public void HienThiThongTinSach()
+    {
+        if (sachList.Count == 0)
+        {
+            Console.WriteLine("Thu vien trong.");
+            return;
+        }
+
+        Console.WriteLine("Danh sach trong thu vien:");
+        foreach (Sach sach in sachList)
+        {
+            Console.WriteLine($"Ma sach: {sach.MaSach}, Ten sach: {sach.TenSach}, Tac gia: {sach.TacGia}");
+        }
+    }
+}
